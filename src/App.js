b5e5e6847bef5bb663533d7568f63e93049f49fd
mKnopfPortfolio <mknopfportfolio@gmail.com>
2022-05-31 18:57:30 -0400

    add styling to about section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import AboutUs from "./pages/AboutUs";
+import GlobalStyle from "./components/GlobalStyle";
 
 function App() {
   return (
     <div className="App">
       <h1>film.</h1>
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
