426548146cf3a7c8cb6dde25cd50d8932c11f576
mKnopfPortfolio <mknopfportfolio@gmail.com>
2022-06-01 17:44:52 -0400

    add react-dom and links

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import AboutUs from "./pages/AboutUs";
 import GlobalStyle from "./components/GlobalStyle";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
 import Nav from "./components/Nav";
+import {Route, Routes} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Routes>
+        <Route path="/" exact element={<AboutUs/>}/>
+        <Route path="/aboutus" exact element={<AboutUs/>}/>
+        <Route path="/ourwork" exact element={<OurWork/>}/>
+        <Route path="/contactus" exact element={<ContactUs/>}/>
+      </Routes>
     </div>
   );
 }
